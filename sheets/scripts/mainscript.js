3d5a3c542606265dbea5b6d8f03f0e4af950cb91
AnirudhVijayaraghavan <114264437+AnirudhVijayaraghavan@users.noreply.github.com>
2022-12-21 00:07:53 -0500

    10th commit.
    -Added media queries.
    -Added different styles to align section headers properly.
    -Fixed misalignment with index.html.
    -Fixed footer not adjusting text to center on opening navbar.
    -Fixed media queries not hiding hamburger icon properly.

--- sheets/scripts/mainscript.js
+++ sheets/scripts/mainscript.js
@@ -5,49 +5,49 @@ const footerarea = document.querySelector("footer");
 const pageProgressBar = document.querySelector(".progress-bar");
 
 const scrollContainer = () => {
-    return document.documentElement || document.body;
+  return document.documentElement || document.body;
 };
 
 const goToHomePage = () => {
-    window.location.href = "../index.html";
+  window.location.href = "../index.html";
 };
 
 document.addEventListener("scroll", () => {
-    console.log("Scroll Height: ", scrollContainer().scrollHeight);
-    console.log("Client Height: ", scrollContainer().clientHeight);
-    const scrolledPercentage =
-        (scrollContainer().scrollTop /
-            (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
-        100;
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
 
-    pageProgressBar.style.width = `${scrolledPercentage}%`;
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
 
-    if (scrollContainer().scrollTop > showOnPx) {
-        backToTopButton.classList.remove("hidden");
-        footerarea.classList.remove("hidden");
-    } else {
-        backToTopButton.classList.add("hidden");
-        footerarea.classList.add("hidden");
-    }
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+    footerarea.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+    footerarea.classList.add("hidden");
+  }
 });
 
 backToTopButton.addEventListener("click", goToHomePage);
 
 // Code to embed google map API.
 function myMap() {
-    // The location of Walnut avenue.
-    const walnut = { lat: 42.312312864693176, lng: -71.09708490000003 };
-    // The map, centered at Walnut avenue.
-    const map = new google.maps.Map(document.getElementById("googleMap"), {
-        zoom: 12,
-        center: walnut,
-    });
-    // The marker, positioned at Walnut avenue.
-    const marker = new google.maps.Marker({
-        position: walnut,
-        map: map,
-    });
-    window.initMap = initMap;
+  // The location of Walnut avenue.
+  const walnut = { lat: 42.312312864693176, lng: -71.09708490000003 };
+  // The map, centered at Walnut avenue.
+  const map = new google.maps.Map(document.getElementById("googleMap"), {
+    zoom: 12,
+    center: walnut,
+  });
+  // The marker, positioned at Walnut avenue.
+  const marker = new google.maps.Marker({
+    position: walnut,
+    map: map,
+  });
+  window.initMap = initMap;
 }
 
 // Code to include ScrollFade.js functionality.
@@ -60,9 +60,9 @@ function scrollFade() {
     var element = fadeElements[index];
     var rect = element.getBoundingClientRect();
 
-    var elementFourth = rect.height/4;
+    var elementFourth = rect.height / 4;
     var fadeInPoint = window.innerHeight - elementFourth;
-    var fadeOutPoint = -(rect.height/2);
+    var fadeOutPoint = -(rect.height / 2);
 
     if (rect.top <= fadeInPoint) {
       element.classList.add('scrollFade--visible');
@@ -82,6 +82,49 @@ function scrollFade() {
 
 document.addEventListener('scroll', scrollFade);
 window.addEventListener('resize', scrollFade);
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   scrollFade();
 });
+
+// The following code is for the navbar collapse.
+let state = "closed";
+function openNav() {
+  state = "open";
+  document.getElementById("mySidebar").style.width = "20%";
+  document.getElementById("main").style.marginLeft = "20%";
+  document.getElementsByClassName("openbtn")[0].style.display = "none";
+  document.getElementsByClassName("openbtn")[0].style.opacity = "100%";
+  document.getElementsByTagName("footer")[0].style.width = "80%";
+
+}
+
+function closeNav() {
+  state = "closed";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementsByClassName("openbtn")[0].style.display = "inline";
+  document.getElementsByTagName("footer")[0].style.width = "100%";
+}
+
+function openorclose() {
+  if (state === "open") {
+    closeNav();
+  }
+  else if (state === "closed") {
+    openNav();
+  }
+}
+
+// The following is to fix the bug where hamburger icon does not respond to media query if used
+// even once.
+function myFunction(x) {
+  if (x.matches) { 
+    document.getElementsByClassName("openbtn")[0].style.display = "none";
+  } else {
+    document.getElementsByClassName("openbtn")[0].style.display = "inline";
+  }
+}
+
+var x = window.matchMedia("(max-width: 800px)")
+myFunction(x)
+x.addListener(myFunction) 
